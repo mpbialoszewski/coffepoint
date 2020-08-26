c4bdb4d42053f929f87b2056935a3ec3d084437a
Mariusz Bialoszewski <m.p.bialoszewski@outlook.com>
2020-08-26 16:27:11 +0100

    Adding Semantic UI part 2

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,7 +1,5 @@
 const { environment } = require('@rails/webpacker')
 
-module.exports = environment
-
 const webpack = require("webpack")
 
 environment.plugins.append("Provide", new webpack.ProvidePlugin({
@@ -9,3 +7,6 @@ environment.plugins.append("Provide", new webpack.ProvidePlugin({
   jQuery: 'jquery',
   Popper: ['popper.js', 'default']
 }))
+
+module.exports = environment
+
