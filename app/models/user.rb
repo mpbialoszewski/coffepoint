0b6eb2b963312f701de71114aae023471745e174
Mariusz Bialoszewski <m.p.bialoszewski@outlook.com>
2020-08-27 11:57:10 +0100

    Adding message model and resources

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  validates :username, presence: true, length: { minimum: 3, maximum: 15 }
+  validates :username, presence: true, length: { minimum: 3, maximum: 15 },
+            uniqueness: { case_sensitive: false }
+  has_many :messages
   has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_26_161322) do
+ActiveRecord::Schema.define(version: 2020_08_27_105513) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "messages", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
